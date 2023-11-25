9044f851dbd3721823b0b250dd7e485c6c7f446c
claralonguet <151842131+claralonguet@users.noreply.github.com>
2023-11-25 17:30:37 +0100

    Update utility.c

--- utility.c
+++ utility.c
@@ -11,4 +11,22 @@
  * @return a pointer to the resulting path, NULL when concatenation failed
  */
 char *concat_path(char *result, char *prefix, char *suffix) {
+   // Copier le préfixe dans le résultat
+    strcpy(result, prefix);
+
+    // Vérifier si le préfixe se termine par un '/'
+    if (result[strlen(result) - 1] != '/') {
+        // Ajouter un '/' si nécessaire
+        strcat(result, "/");
+        }
+
+    // Concaténer le suffixe au résultat
+    strcat(result, suffix);
+
+    // Vérifier la longueur du résultat
+    if (strlen(result) >= PATH_SIZE) {
+        return NULL; // Le résultat dépasse la taille maximale
+    }
+
+    return result;
 }
