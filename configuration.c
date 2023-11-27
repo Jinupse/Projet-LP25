d36d948b2f10c479d0aa2f767f332d83d4aea203
IselineCastel <151844654+IselineCastel@users.noreply.github.com>
2023-11-27 03:17:48 +0100

    Update configuration.c

--- configuration.c
+++ configuration.c
@@ -24,7 +24,36 @@ void display_help(char *my_name) {
  * @brief init_configuration initializes the configuration with default values
  * @param the_config is a pointer to the configuration to be initialized
  */
-void init_configuration(configuration_t *the_config) {
+void init_configuration(configuration_t *the_config) 
+//acquisition des paramètres
+printf("Entrez les paramètres suivants : ");
+
+printf("chemin du fichier source : ");
+scanf("%c", the_config->source);
+
+printf("chemin du fichier destination: ");
+scanf("%c", the_config->destination);
+
+printf("nombre de processus analyseurs: ");
+scanf("%hhi",&the_config->processes_count);
+
+printf("exécution de tous les programmes dans un seul proccesus (1 ou 0): ");
+scanf("%d", &the_config->is_parallel);
+
+//vérification avec message d'erreur
+while (the_config->is_parallel!=0 || the_config->is_parallel!=1){
+    printf("Erreur entrez 1 ou 0");
+    printf("exécution de tous les programmes dans un seul proccesus (1 ou 0): ");
+    scanf("%d", &the_config->is_parallel);
+}
+
+printf("utiliser la somme MD5 (1 ou 0): ");
+scanf("%d", &the_config->uses_md5);
+while (the_config->uses_md5!=0 || the_config->uses_md5!=1){
+    printf("Erreur entrez 1 ou 0");
+    printf("utiliser la somme MD5 (1 ou 0): ");
+    scanf("%d", &the_config->uses_md5);
+}
 }
 
 /*!
@@ -34,5 +63,44 @@ void init_configuration(configuration_t *the_config) {
  * @param argv is an array of strings with the program parameters
  * @return -1 if configuration cannot succeed, 0 when ok
  */
-int set_configuration(configuration_t *the_config, int argc, char *argv[]) {
-}
+int set_configuration(configuration_t *the_config, int argc, char *argv[]) struct option my_opts[] = {
+        {.name="source", .has_arg=1, .flag=0, .val='s'},
+        {.name="destination", .has_arg=1, .flag=0, .val='d'},
+        {.name="processes-count", .has_arg=1, .flag=0, .val='p'},
+        {.name="is_parallel", .has_arg=0, .flag=0, .val='i'},
+        {.name="uses_md5", .has_arg=0, .flag=0, .val='m'},
+        {.name=0, .has_arg=0, .flag=0, .val=0},
+    };
+int opt;
+    while ((opt = getopt_long(argc, argv, "", my_opts, NULL)) != -1) {
+        switch (opt) {
+            case 's':
+                // Update source path in the configuration
+                snprintf(the_config->source, sizeof(the_config->source), "%s", optarg);
+                break;
+
+            case 'd':
+                // Update destination path in the configuration
+                snprintf(the_config->destination, sizeof(the_config->destination), "%s", optarg);
+                break;
+
+            case 'p':
+                // Update processes count in the configuration
+                the_config->processes_count = atoi(optarg);
+                break;
+
+            case 'i':
+                // Update is_parallel flag in the configuration
+                the_config->is_parallel = 1;
+                break;
+
+            case 'm':
+                // Update uses_md5 flag in the configuration
+                the_config->uses_md5 = 1;
+                break;
+
+            default:
+                // Handle unknown or invalid options
+                return -1;
+        }
+    }
