7ef556f1eea91f2621adc0e2fdc90c7b03ebc4f4
IselineCastel <151844654+IselineCastel@users.noreply.github.com>
2023-12-30 23:13:49 +0100

    Update messages.c

--- messages.c
+++ messages.c
@@ -37,6 +37,7 @@ int send_analyze_dir_command(int msg_queue, int recipient, char *target_dir) {
  * Calls send_file_entry function
  */
 int send_analyze_file_command(int msg_queue, int recipient, files_list_entry_t *file_entry) {
+   return send_file_entry(msg_queue, recipient, file_entry);
 }
 
 /*!
@@ -48,6 +49,7 @@ int send_analyze_file_command(int msg_queue, int recipient, files_list_entry_t *
  * Calls send_file_entry function
  */
 int send_analyze_file_response(int msg_queue, int recipient, files_list_entry_t *file_entry) {
+   return send_file_entry(msg_queue, recipient, file_entry); }
 }
 
 /*!
@@ -59,6 +61,7 @@ int send_analyze_file_response(int msg_queue, int recipient, files_list_entry_t 
  * Calls send_file_entry function
  */
 int send_files_list_element(int msg_queue, int recipient, files_list_entry_t *file_entry) {
+   return send_file_entry(msg_queue, recipient, file_entry); }
 }
 
 /*!
